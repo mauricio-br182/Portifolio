ca7d791b4b46c8a36d2bf9f371d2039d56567ebe
Mauricio <mauriciobrdev@gmail.com>
2022-12-27 00:44:56 -0300

    content modifyed

--- src/pages/projetos/index.tsx
+++ src/pages/projetos/index.tsx
@@ -66,30 +66,6 @@ export default function Projetos(){
                         </span> 
                         </a>
                     </div>
-                    <div className={styles.projContent}>
-                        <Image 
-                            src={ProjectImage} 
-                            width={290} 
-                            height={180} 
-                            quality={100} 
-                            placeholder='blur'
-                            blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mN8cpenHgAGyAJOaI3nMAAAAABJRU5ErkJggg=="
-                            alt="Projeto barbearia" 
-                        />
-                        <div className={styles.techs}>
-                            <span>Javascript</span>
-                            <span>HTML</span>
-                            <span>CSS</span>
-                        </div>
-                        <span className={styles.spanName}>Nome do Projeto</span>
-                        <h2>Ambient-Barbearia</h2>
-                        <a href="https://ambientbarbearia.vercel.app/">
-                        <span>
-                            Ver Projeto 
-                        </span> 
-                        </a>
-                    </div>
-                    
                 </section>
             </main>
         </>
